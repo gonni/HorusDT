9020d2df3677ad52adbe94f744f06b3302d34e82
김영곤 <jeff.y.kim@onestorecorp.com>
2022-08-30 17:12:55 +0900

    daily commit

--- conf/dag-spark-horus-tdm.py
+++ conf/dag-spark-horus-tdm.py
@@ -24,16 +24,16 @@ now = datetime.now()
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2022, 8, 29),
+    "start_date": datetime(2022, 8, 30),
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 1,
+    "retries": 0,
     "retry_delay": timedelta(minutes=1)
 }
 
 dag = DAG(
-    dag_id="spark-horus-tdm-30m",
+    dag_id="spark-horus-tdm-1h",
     description="spark ml term-distance-matrix processing",
     default_args=default_args,
     schedule_interval='*/1 * * * *'
@@ -46,12 +46,13 @@ spark_config = {
         "spark.master":spark_master
     },
     'conn_id': 'spark_default',
+    'num_executors': 1,
     'driver_class_path': driver_class_path,
     'application': '/usr/local/spark/resources/horus/HorusDT-assembly-0.1.3-SNAPSHOT.jar',
     'java_class': 'com.yg.horus.dt.tdm.TdmJobMain'
 }
 
-operator = SparkSubmitOperator(task_id='spark-horus-tdm-write-per30m', dag=dag, **spark_config)
+operator = SparkSubmitOperator(task_id='spark-horus-tdm-write-per1h', dag=dag, **spark_config)
 
 end = DummyOperator(task_id="end", dag=dag)
 
