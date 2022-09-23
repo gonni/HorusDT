833b4e6d398da4286b4dcfa5661721b8e525310d
김영곤 <jeff.y.kim@onestorecorp.com>
2022-09-23 16:34:37 +0900

    daily commit

--- src/main/scala/com/yg/horus/dt/tdm/WordVectorAnalyzer.scala
+++ src/main/scala/com/yg/horus/dt/tdm/WordVectorAnalyzer.scala
@@ -37,8 +37,20 @@ object WordVectorAnalyzer extends SparkJobInit("W2vAnalyzer") {
 
   def searchByRelation(positiveWord: String, positiveWord2: String, num: Int): DataFrame = {
     val target = word2DM(positiveWord).add(word2DM(positiveWord2))
-    val result = model.findSynonyms(Vectors.dense(target.toArray), num)
+    val target2 = target.div(2)
+    println("Added Vectors -> " + Vectors.dense(target.toArray))
+    println("Divi Vectors -> " + Vectors.dense(target2.toArray))
+    val result = model.findSynonyms(Vectors.dense(target2.toArray), num)
     result
+  }
+
+
+  def nearTermsOnVector(srcTerms : Seq[String], limit: Int) = {
+//    val sumVector = srcTerms.reduce((a1, a2) => word2DM(a1).add(word2DM(a2)))
+    val sumVector = srcTerms.foldLeft(word2DM(srcTerms(0)))((a, b) => a.add(word2DM(b)))
+    val centerVector = sumVector.div(srcTerms.length)
+
+    model.findSynonyms(Vectors.dense(centerVector.toArray), limit)
   }
 
   def searchByRelation(positiveWord: String, negativeWord: String, positiveWord2: String, num: Int): DataFrame = {
@@ -58,11 +70,14 @@ object WordVectorAnalyzer extends SparkJobInit("W2vAnalyzer") {
   def main(args: Array[String]): Unit = {
     println("Active System ..")
 
-    val positive_word_01 = "일본"
+    val positive_word_01 = "우주"
 //    val negative_word_02 = "대한민국"
-    val positive_word_03 = "중국"
-    val result = searchByRelation(positive_word_01,  positive_word_03, 10)
-    result.show()
+    val positive_word_03 = "미국"
+//    val result = searchByRelation(positive_word_01,  positive_word_03, 10)
+//    result.show()
+
+    val src = Seq("중국", "우주")
+    nearTermsOnVector(src, 10) show 20
 
   }
 
